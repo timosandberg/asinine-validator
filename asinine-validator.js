02036f57fdf8e4419f70ee2034530acf3c2b20bc
Timo Sandberg <warren@iki.fi>
2014-10-11 18:09:12 +0300

    jslint cleanup

--- asinine-validator.js
+++ asinine-validator.js
@@ -1,3 +1,7 @@
+/*jslint white:true*/
+
+"use strict";
+
 /**
  * Asinine Validator
  *
@@ -7,48 +11,43 @@
  * @author: Timo Sandberg <warren@iki.fi>
  *
  */
-var Validator = function() {
+var Validator = function () {
 	this.validators = {};
 
-	this.init = function() {
-        this.validators = {
-            username: this.factory({ pattern: /^[a-zA-Z0-9_\-]{4,16}$/, min: 4, max: 16, required: true }),
-            email: this.factory({ pattern: /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/, required: true, message: "Enter valid e-mail address" }),
-            password: this.factory({ pattern: /^[a-zA-Z0-9_#+\-!%&?]{6,32}$/, min: 6, max: 32, required: true }),
-            text: this.factory({ pattern: /^[\s\wäöÄÖÅå%&#]{3,20}$/, min: 3, max: 20, required: true }),
-            number: this.factory({ pattern: /^(0|[1-9]\d{0,5})$/, min: 1, max: 6, required: true, message: "Enter an integer"})
-        };
+	this.init = function () {
+		this.validators = {
+			username: this.factory({ pattern: /^[a-zA-Z0-9_\-]{4,16}$/, min: 4, max: 16, required: true }),
+			email: this.factory({ pattern: /^[_a-z0-9\-]+(\.[_a-z0-9\-]+)*@[a-z0-9\-]+(\.[a-z0-9\-]+)*(\.[a-z]{2,4})$/, required: true, message: "Enter valid e-mail address" }),
+			password: this.factory({ pattern: /^[a-zA-Z0-9_#+\-!%&?]{6,32}$/, min: 6, max: 32, required: true }),
+			text: this.factory({ pattern: /^[\s\wäöÄÖÅå%&#]{3,20}$/, min: 3, max: 20, required: true }),
+			number: this.factory({ pattern: /^(0|[1-9]\d{0,5})$/, min: 1, max: 6, required: true, message: "Enter an integer"})
+		};
 	};
 
-    this.factory = function (opts) {
-		opts.pattern = (typeof opts.pattern == "string" ? new RegExp(opts.pattern) : opts.pattern);
+	this.factory = function (opts) {
+		opts.pattern = (typeof opts.pattern === "string" ? new RegExp(opts.pattern) : opts.pattern);
 
-        return function (tainted) {
-            if (opts.pattern.test(tainted)) {
-                return true;
-            } else {
-				if (!opts.required && tainted.length == 0) return true;
-                if (opts.required && tainted.length < 1) return "Required field";
-                else if (opts.min && tainted.length < opts.min) return "Minimum " + opts.min + " characters";
-                else if (opts.max && tainted.length > opts.max) return "Maximum " + opts.max + " characters";
-                else return opts.message || "Invalid value";
-            }
-            return false;
-        }
+		return function (tainted) {
+            if (opts.pattern.test(tainted)) { return true; } 
+			if (!opts.required && tainted.length === 0) { return true; }
+            if (opts.required && tainted.length < 1) { return "Required field"; }
+        	if (opts.min && tainted.length < opts.min) { return "Minimum " + opts.min + " characters"; }
+    	    if (opts.max && tainted.length > opts.max) { return "Maximum " + opts.max + " characters"; }
+  	        return opts.message || "Invalid value";
+        };
     };
-    this.validate = function(validator, value) {
-		if (this.validators[validator])
+    this.validate = function (validator, value) {
+		if (this.validators[validator]) {
 	        return this.validators[validator](value);
-		else
-			return false;
+		} 
+		return false;
     };
-	this.add = function(opts) {
+	this.add = function (opts) {
 		if (opts.name && opts.pattern) {
 			this.validators[opts.name] = this.factory(opts);
 			return true;
-		} else {
-			return false;
 		}
+		return false;
 	};
 	this.init();
 };
